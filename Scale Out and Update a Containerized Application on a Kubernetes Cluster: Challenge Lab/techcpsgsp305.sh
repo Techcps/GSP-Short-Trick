8f6030f0045e0310dbacba7748c5e054c0ba9a08
Techcps <104138529+Techcps@users.noreply.github.com>
2024-01-25 23:36:02 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Scale Out and Update a Containerized Application on a Kubernetes Cluster: Challenge Lab/techcpsgsp305.sh
@@ -0,0 +1,16 @@
+
+
+gsutil cp gs://$DEVSHELL_PROJECT_ID/echo-web-v2.tar.gz .
+tar -xzvf echo-web-v2.tar.gz
+
+gcloud builds submit --tag gcr.io/$DEVSHELL_PROJECT_ID/echo-app:v2 .
+
+gcloud container clusters get-credentials echo-cluster --zone=$ZONE
+
+kubectl create deployment echo-web --image=gcr.io/qwiklabs-resources/echo-app:v2
+
+kubectl expose deployment echo-web --type=LoadBalancer --port 80 --target-port 8000
+
+kubectl scale deploy echo-web --replicas=2
+
+kubectl port-forward service/echo-web 8080:80
