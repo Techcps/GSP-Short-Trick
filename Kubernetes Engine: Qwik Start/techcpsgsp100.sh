36f7956c5f819d7ec446fb0527192b1bcac79931
Techcps <104138529+Techcps@users.noreply.github.com>
2024-01-29 12:12:59 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Kubernetes Engine: Qwik Start/techcpsgsp100.sh
+++ Kubernetes Engine: Qwik Start/techcpsgsp100.sh
@@ -1,14 +1,18 @@
 
 
 gcloud auth list
+
 gcloud config list project
-export REGION="${ZONE%-*}"
-gcloud config set compute/region $REGION
-gcloud config set compute/zone $ZONE
+
 gcloud container clusters create --machine-type=e2-medium --zone=$ZONE lab-cluster
+
 gcloud container clusters get-credentials lab-cluster
+
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
+
 kubectl expose deployment hello-server --type=LoadBalancer --port 8080
+
 kubectl get service
+
 gcloud container clusters delete lab-cluster
 
