62cf9bf717328c0b83aeefd1590abddcc6db5c72
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-03 20:35:53 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Troubleshooting Common SQL Errors with BigQuery/techcpsgsp408.sh
+++ Troubleshooting Common SQL Errors with BigQuery/techcpsgsp408.sh
@@ -2,16 +2,37 @@
 
 bq query --use_legacy_sql=false '#standardSQL
 SELECT
+fullVisitorId
+FROM `data-to-insights.ecommerce.rev_transactions`'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT fullVisitorId hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
 COUNT(DISTINCT fullVisitorId) AS visitor_count
 , hits_page_pageTitle
-FROM data-to-insights.ecommerce.rev_transactions
+FROM `data-to-insights.ecommerce.rev_transactions`
 GROUP BY hits_page_pageTitle'
 
 bq query --use_legacy_sql=false '#standardSQL
 SELECT
 COUNT(DISTINCT fullVisitorId) AS visitor_count
 , hits_page_pageTitle
-FROM data-to-insights.ecommerce.rev_transactions
+FROM `data-to-insights.ecommerce.rev_transactions`
 WHERE hits_page_pageTitle = "Checkout Confirmation"
 GROUP BY hits_page_pageTitle'
 
@@ -21,7 +42,7 @@ geoNetwork_city,
 SUM(totals_transactions) AS totals_transactions,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors
 FROM
-data-to-insights.ecommerce.rev_transactions
+`data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city'
 
 bq query --use_legacy_sql=false '#standardSQL
@@ -30,7 +51,7 @@ geoNetwork_city,
 SUM(totals_transactions) AS totals_transactions,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors
 FROM
-data-to-insights.ecommerce.rev_transactions
+`data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city
 ORDER BY distinct_visitors DESC'
 
@@ -41,7 +62,7 @@ SUM(totals_transactions) AS total_products_ordered,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors,
 SUM(totals_transactions) / COUNT( DISTINCT fullVisitorId) AS avg_products_ordered
 FROM
-data-to-insights.ecommerce.rev_transactions
+`data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city
 ORDER BY avg_products_ordered DESC'
 
@@ -52,18 +73,17 @@ SUM(totals_transactions) AS total_products_ordered,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors,
 SUM(totals_transactions) / COUNT( DISTINCT fullVisitorId) AS avg_products_ordered
 FROM
-data-to-insights.ecommerce.rev_transactions
+`data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city
 HAVING avg_products_ordered > 20
 ORDER BY avg_products_ordered DESC'
+
 bq query --use_legacy_sql=false '#standardSQL
 SELECT
 COUNT(DISTINCT hits_product_v2ProductName) as number_of_products,
 hits_product_v2ProductCategory
-FROM data-to-insights.ecommerce.rev_transactions
+FROM `data-to-insights.ecommerce.rev_transactions`
 WHERE hits_product_v2ProductName IS NOT NULL
 GROUP BY hits_product_v2ProductCategory
 ORDER BY number_of_products DESC
 LIMIT 5'
-
-
