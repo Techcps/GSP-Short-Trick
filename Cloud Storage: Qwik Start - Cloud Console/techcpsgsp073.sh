8b9f55a61bac0bb9c001c68b4228a6156a3a2c51
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-03 19:28:18 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Cloud Storage: Qwik Start - Cloud Console/techcpsgsp073.sh
+++ Cloud Storage: Qwik Start - Cloud Console/techcpsgsp073.sh
@@ -1,6 +1,7 @@
 
 gsutil mb -l $REGION -c Standard gs://$DEVSHELL_PROJECT_ID
 
+curl -o https://github.com/Techcps/GSP-Short-Trick/blob/main/Cloud%20Storage%3A%20Qwik%20Start%20-%20Cloud%20Console/kitten.png
 
 gsutil cp kitten.png gs://$DEVSHELL_PROJECT_ID/kitten.png
 
