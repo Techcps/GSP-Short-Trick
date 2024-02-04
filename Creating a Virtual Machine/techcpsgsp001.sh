174bb694291123e8b7cd75920e5be8062bf0d309
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-05 02:16:17 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Creating a Virtual Machine/techcpsgsp001.sh
+++ Creating a Virtual Machine/techcpsgsp001.sh
@@ -5,8 +5,4 @@ gcloud compute instances create gcelab2 --machine-type e2-medium --zone=$ZONE
 
 gcloud compute firewall-rules create allow-http --action=ALLOW --direction=INGRESS --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
 
-gcloud compute ssh gcelab --zone=$ZONE --quiet
-
-sudo apt-get update
-sudo apt-get install -y nginx
-ps auwx | grep nginx
+gcloud compute ssh gcelab --zone=$ZONE --quiet --command "sudo apt-get update && sudo apt-get install -y nginx && ps auwx | grep nginx "
