662faa7918677385230d31f515347745ca94e28a
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-04 05:35:07 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Working with the Google Cloud Console and Cloud Shell/techcps.sh
+++ Working with the Google Cloud Console and Cloud Shell/techcps.sh
@@ -1,12 +1,13 @@
 
+
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 export BUCKET_NAME=$PROJECT_ID
 
 gsutil mb gs://$PROJECT_ID
 
-gcloud storage buckets create gs://$PROJECT_ID-BUCKET
+gsutil mb -p $PROJECT_ID gs://$PROJECT_ID-first-bucket
 
 curl -O https://github.com/Techcps/GSP-Short-Trick/blob/main/Cloud%20Storage%3A%20Qwik%20Start%20-%20Cloud%20Console/kitten.png
 
-gsutil cp kitten.png gs://$PROJECT_ID-BUCKET/kitten.png
+gsutil cp kitten.png gs://$PROJECT_ID-first-bucket/kitten.png
 
