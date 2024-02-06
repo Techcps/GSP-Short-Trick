104d942861a6ea9388f8f010e2d24d2b37fb2b88
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-07 02:31:04 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Working with Virtual Machines/techcps.sh
+++ Working with Virtual Machines/techcps.sh
@@ -12,7 +12,3 @@ gcloud compute firewall-rules create minecraft-rule --project=$PROJECT_ID --targ
 
 gcloud compute instances add-metadata mc-server --zone=$ZONE --metadata project-id=$PROJECT_ID
 
-gcloud compute ssh mc-server --zone=$ZONE --quiet --command "sudo mkdir -p /home/minecraft && sudo mkfs.ext4 -F -E lazy_itable_init=0,\
-lazy_journal_init=0,discard \
-/dev/disk/by-id/google-minecraft-disk && sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft && sudo apt-get update && sudo apt-get install -y default-jre-headless && cd /home/minecraft && sudo apt-get install wget -y && sudo wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar && sudo java -Xmx1024M -Xms1024M -jar server.jar nogui"
-
