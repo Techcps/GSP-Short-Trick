9cdacf74c5f8c8ca38720a30fc1787bbe144d6e1
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-06 17:38:13 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Working with Virtual Machines/techcps.sh
+++ Working with Virtual Machines/techcps.sh
@@ -14,12 +14,6 @@ gcloud compute instances add-metadata mc-server --zone=$ZONE --metadata project-
 
 gcloud compute ssh mc-server --zone=$ZONE --quiet --command "sudo mkdir -p /home/minecraft && sudo mkfs.ext4 -F -E lazy_itable_init=0,\
 lazy_journal_init=0,discard \
-/dev/disk/by-id/google-minecraft-disk && sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft && sudo apt-get update && sudo apt-get install -y default-jre-headless && cd /home/minecraft && sudo apt-get install wget -y && sudo wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar && sudo java -Xmx1024M -Xms1024M -jar server.jar nogui && cat > eula.txt <<'EOF_END'
-GNU nano 5.4 eula.txt *
-#By changing the setting below to TRUE you are indicating your agreement to our>
-#Tue Feb 06 06:45:46 UTC 2024
-eula=true
-EOF_END && sudo apt-get install -y screen && sudo screen -S mcs java -Xmx1024M -Xms1024M -jar server.jar nogui && sudo screen -r mcs && exit"
+/dev/disk/by-id/google-minecraft-disk && sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft && sudo apt-get update && sudo apt-get install -y default-jre-headless && cd /home/minecraft && sudo apt-get install wget -y && sudo wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar && sudo java -Xmx1024M -Xms1024M -jar server.jar nogui && echo "eula=true" | sudo tee eula.txt && sudo apt-get install -y screen && sudo screen -S mcs java -Xmx1024M -Xms1024M -jar server.jar nogui && sudo screen -r mcs && exit"
 
-gcloud compute ssh mc-server --zone=$ZONE --quiet --command "export PROJECT_ID=$(gcloud config get-value project) && export BUCKET_NAME=$PROJECT_ID && echo $PROJECT_ID && gcloud storage buckets create gs://$PROJECT_ID-minecraft-backup && echo BUCKET_NAME=$PROJECT_ID >> ~/.profile && cd /home/minecraft"
-
+gcloud compute ssh mc-server --zone=$ZONE --quiet --command "export PROJECT_ID=$(gcloud config get-value project) && export BUCKET_NAME=$PROJECT_ID && echo $PROJECT_ID && gcloud storage buckets create gs://$PROJECT_ID-minecraft-backup && echo BUCKET_NAME=$PROJECT_ID >> ~/.profile"
