ccc2009d92a9c6b7bb44b0a0a68bbac83bda4015
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-17 21:30:06 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- BigQuery: Qwik Start - Command Line/techcpsgsp071.sh
+++ BigQuery: Qwik Start - Command Line/techcpsgsp071.sh
@@ -13,8 +13,7 @@ bq query --use_legacy_sql=false \
    word'
 
 bq query --use_legacy_sql=false \
-"
-SELECT
+'SELECT
    word
  FROM
    `bigquery-public-data`.samples.shakespeare
@@ -32,4 +31,3 @@ bq load babynames.names2010 yob2010.txt name:string,gender:string,count:integer
 bq query "SELECT name,count FROM babynames.names2010 WHERE gender = 'F' ORDER BY count DESC LIMIT 5"
 
 bq query "SELECT name,count FROM babynames.names2010 WHERE gender = 'M' ORDER BY count ASC LIMIT 5"
-
