9cf8368c97c59b136dcfd3ffb2fb186e58084864
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-21 10:17:20 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Automating the Deployment of Infrastructure Using Terraform/mynetwork.tf
+++ Automating the Deployment of Infrastructure Using Terraform/mynetwork.tf
@@ -1,34 +1,39 @@
 # Create the mynetwork network
 resource "google_compute_network" "mynetwork" {
-  name = "mynetwork"
-  # RESOURCE properties go here
-  auto_create_subnetworks = "true"
+  name                    = "mynetwork"
+  auto_create_subnetworks = true
 }
-# Add a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
+
+# Create a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
 resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
-  name = "mynetwork-allow-http-ssh-rdp-icmp"
-  # RESOURCE properties go here
+  name    = "mynetwork-allow-http-ssh-rdp-icmp"
   network = google_compute_network.mynetwork.self_link
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+
   allow {
     protocol = "tcp"
     ports    = ["22", "80", "3389"]
   }
+
   allow {
     protocol = "icmp"
   }
-  source_ranges = ["0.0.0.0/0"]
 }
+
 # Create the mynet-us-vm instance
 module "mynet-us-vm" {
-  source           = "./instance"
-  instance_name    = "mynet-us-vm"
-  instance_zone    = "us-central1-a"
-  instance_network = google_compute_network.mynetwork.self_link
+  source              = "./instance"
+  instance_name       = "mynet-us-vm"
+  instance_zone       = "us-central1-a"
+  instance_subnetwork = google_compute_network.mynetwork.self_link
 }
+
 # Create the mynet-eu-vm" instance
 module "mynet-eu-vm" {
-  source           = "./instance"
-  instance_name    = "mynet-eu-vm"
-  instance_zone    = "europe-west1-d"
-  instance_network = google_compute_network.mynetwork.self_link
+  source              = "./instance"
+  instance_name       = "mynet-eu-vm"
+  instance_zone       = "europe-west1-d"
+  instance_subnetwork = google_compute_network.mynetwork.self_link
 }
