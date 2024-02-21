a0d8a009192ab431bb631f605d3cecc8aba5ea3c
Techcps <104138529+Techcps@users.noreply.github.com>
2024-02-21 10:16:22 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Automating the Deployment of Infrastructure Using Terraform/instance/main.tf
+++ Automating the Deployment of Infrastructure Using Terraform/instance/main.tf
@@ -1,15 +1,26 @@
+variable "instance_name" {}
+variable "instance_zone" {}
+
+variable "instance_type" {
+  default = "n1-standard-1"
+}
+
+variable "instance_subnetwork" {}
+
 resource "google_compute_instance" "vm_instance" {
-  name = "${var.instance_name}"
-  # RESOURCE properties go here
+  name         = "${var.instance_name}"
   zone         = "${var.instance_zone}"
   machine_type = "${var.instance_type}"
+
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      }
+      image = "debian-cloud/debian-10"
+    }
   }
-    network_interface {
-    network = "${var.instance_network}"
+
+  network_interface {
+    subnetwork = "${var.instance_subnetwork}"
+
     access_config {
       # Allocate a one-to-one NAT IP to the instance
     }
