f5e16d0f31de8a408c9a45e69e29e73ee9999751
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-02 03:29:54 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Arcade Hero: Enter the Inbound Firewall ARC140/techcps.sh
+++ Arcade Hero: Enter the Inbound Firewall ARC140/techcps.sh
@@ -1,4 +1,2 @@
 
-gcloud auth list
-export PROJECT_ID=$(gcloud info --format="value(config.project)")
-firewall-rules create default-allow-inbound --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=staging-vm
+gcloud compute firewall-rules create default-allow-inbound --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=staging-vm
