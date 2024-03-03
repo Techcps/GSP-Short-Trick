7e0d362636926ffcb7fa6182c789353a68b6cd85
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-03 22:59:55 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ GKE Workload Optimization/techcps1.sh
@@ -0,0 +1,63 @@
+
+gcloud auth list
+gcloud config list project
+gcloud config set compute/zone $ZONE
+gcloud container clusters create test-cluster --num-nodes=3  --enable-ip-alias
+
+cat << EOF > gb_frontend_pod.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    app: gb-frontend
+  name: gb-frontend
+spec:
+    containers:
+    - name: gb-frontend
+      image: gcr.io/google-samples/gb-frontend-amd64:v5
+      resources:
+        requests:
+          cpu: 100m
+          memory: 256Mi
+      ports:
+      - containerPort: 80
+EOF
+
+kubectl apply -f gb_frontend_pod.yaml
+
+cat << EOF > gb_frontend_cluster_ip.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  name: gb-frontend-svc
+  annotations:
+    cloud.google.com/neg: '{"ingress": true}'
+spec:
+  type: ClusterIP
+  selector:
+    app: gb-frontend
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 80
+EOF
+
+kubectl apply -f gb_frontend_cluster_ip.yaml
+cat << EOF > gb_frontend_ingress.yaml
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: gb-frontend-ingress
+spec:
+  defaultBackend:
+    service:
+      name: gb-frontend-svc
+      port:
+        number: 80
+EOF
+
+kubectl apply -f gb_frontend_ingress.yaml
+BACKEND_SERVICE=$(gcloud compute backend-services list | grep NAME | cut -d ' ' -f2)
+gcloud compute backend-services get-health $BACKEND_SERVICE --global
+sleep 417
+kubectl get ingress gb-frontend-ingress
