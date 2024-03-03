143b7100bd12f2abe7943356f741222fca172362
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-03 23:06:49 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ GKE Workload Optimization/techcps2.sh
@@ -0,0 +1,112 @@
+
+gsutil -m cp -r gs://spls/gsp769/locust-image .
+gcloud builds submit \
+    --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/locust-tasks:latest locust-image
+
+gcloud container images list
+gsutil cp gs://spls/gsp769/locust_deploy_v2.yaml .
+sed 's/${GOOGLE_CLOUD_PROJECT}/'$GOOGLE_CLOUD_PROJECT'/g' locust_deploy_v2.yaml | kubectl apply -f -
+kubectl get service locust-main
+
+cat << EOF > liveness-demo.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    demo: liveness-probe
+  name: liveness-demo-pod
+spec:
+  containers:
+  - name: liveness-demo-pod
+    image: centos
+    args:
+    - /bin/sh
+    - -c
+    - touch /tmp/alive; sleep infinity
+    livenessProbe:
+      exec:
+        command:
+        - cat
+        - /tmp/alive
+      initialDelaySeconds: 5
+      periodSeconds: 10
+EOF
+
+kubectl apply -f liveness-demo.yaml
+kubectl describe pod liveness-demo-pod
+kubectl exec liveness-demo-pod -- rm /tmp/alive
+kubectl describe pod liveness-demo-pod
+
+cat << EOF > readiness-demo.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    demo: readiness-probe
+  name: readiness-demo-pod
+spec:
+  containers:
+  - name: readiness-demo-pod
+    image: nginx
+    ports:
+    - containerPort: 80
+    readinessProbe:
+      exec:
+        command:
+        - cat
+        - /tmp/healthz
+      initialDelaySeconds: 5
+      periodSeconds: 5
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: readiness-demo-svc
+  labels:
+    demo: readiness-probe
+spec:
+  type: LoadBalancer
+  ports:
+    - port: 80
+      targetPort: 80
+      protocol: TCP
+  selector:
+    demo: readiness-probe
+EOF
+
+kubectl apply -f readiness-demo.yaml
+sleep 30
+kubectl get service readiness-demo-svc
+kubectl describe pod readiness-demo-pod
+kubectl exec readiness-demo-pod -- touch /tmp/healthz
+kubectl describe pod readiness-demo-pod | grep ^Conditions -A 5
+kubectl delete pod gb-frontend
+
+cat << EOF > gb_frontend_deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: gb-frontend
+  labels:
+    run: gb-frontend
+spec:
+  replicas: 5
+  selector:
+    matchLabels:
+      run: gb-frontend
+  template:
+    metadata:
+      labels:
+        run: gb-frontend
+    spec:
+      containers:
+        - name: gb-frontend
+          image: gcr.io/google-samples/gb-frontend-amd64:v5
+          resources:
+            requests:
+              cpu: 100m
+              memory: 128Mi
+          ports:
+            - containerPort: 80
+              protocol: TCP
+EOF
