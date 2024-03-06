0d3a5948d8d1fc921ed0de8de4b2ef2b0f2bef4d
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-06 22:58:40 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Interact with Terraform Modules/techcpsgsp751.sh
+++ Interact with Terraform Modules/techcpsgsp751.sh
@@ -24,3 +24,63 @@ terraform apply --auto-approve
 terraform destroy --auto-approve
 cd ~
 rm -rd terraform-google-network -f
+
+
+cd ~
+touch main.tf
+mkdir -p modules/gcs-static-website-bucket
+
+cd modules/gcs-static-website-bucket
+touch website.tf variables.tf outputs.tf
+
+tee -a README.md <<EOF
+# GCS static website bucket
+This module provisions Cloud Storage buckets configured for static website hosting.
+EOF
+
+tee -a LICENSE <<EOF
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+EOF
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Interact%20with%20Terraform%20Modules/website.tf
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Interact%20with%20Terraform%20Modules/variables.tf
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Interact%20with%20Terraform%20Modules/outputs.tf
+cd ~
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Interact%20with%20Terraform%20Modules/main.tf
+
+cat > outputs.tf <<EOF_END
+output "bucket-name" {
+  description = "Bucket names."
+  value       = "module.gcs-static-website-bucket.bucket"
+}
+EOF_END
+
+cat > variables.tf <<EOF_END
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "$DEVSHELL_PROJECT_ID"
+}
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "$DEVSHELL_PROJECT_ID"
+}
+EOF_END
+
+terraform init
+terraform apply --auto-approve
+
+cd ~
+curl https://raw.githubusercontent.com/hashicorp/learn-terraform-modules/master/modules/aws-s3-static-website-bucket/www/index.html > index.html
+curl https://raw.githubusercontent.com/hashicorp/learn-terraform-modules/blob/master/modules/aws-s3-static-website-bucket/www/error.html > error.html
+
+gsutil cp *.html gs://$DEVSHELL_PROJECT_ID
