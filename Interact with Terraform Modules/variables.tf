313c4a6ef8af9e202d92c7d2414c1970bb1159e6
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-08 15:03:58 +0530

    Delete Interact with Terraform Modules/variables.tf

--- Interact with Terraform Modules/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The project ID to host the network in"
-  default     = "$DEVSHELL_PROJECT_ID"
-}
-
-variable "network_name" {
-  description = "The name of the VPC network being created"
-  default     = "example-vpc"
-}
