b0fc0b47471a87b7d62c68c2221a7dc392ffab3e
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-18 07:46:08 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Hello Node Kubernetes/techcpsgsp005.sh
@@ -0,0 +1,54 @@
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+cat > server.js <<EOF_CP
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("Hello World!");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
+EOF_CP
+
+cat > Dockerfile <<EOF_CP
+FROM node:6.9.2
+EXPOSE 8080
+COPY server.js .
+CMD node server.js
+EOF_CP
+
+docker build -t gcr.io/$PROJECT_ID/hello-node:v1 .
+
+docker run -d -p 8080:8080 gcr.io/$PROJECT_ID/hello-node:v1
+
+curl http://localhost:8080
+
+ID=$(docker ps --format '{{.ID}}')
+
+docker stop $ID
+
+gcloud auth configure-docker --quiet
+
+docker push gcr.io/$PROJECT_ID/hello-node:v1
+
+gcloud config set project $PROJECT_ID
+
+gcloud container clusters create hello-world --zone="$ZONE" --num-nodes 2 --machine-type n1-standard-1
+
+kubectl create deployment hello-node --image=gcr.io/$PROJECT_ID/hello-node:v1
+sleep 5
+kubectl get deployments
+sleep 5
+kubectl get pods
+kubectl cluster-info
+kubectl config view
+kubectl get events
+kubectl expose deployment hello-node --type="LoadBalancer" --port=8080
+sleep 7
+kubectl get services
+kubectl scale deployment hello-node --replicas=4
+sleep 5
+kubectl get deployment
+sleep 7
+kubectl get pods
