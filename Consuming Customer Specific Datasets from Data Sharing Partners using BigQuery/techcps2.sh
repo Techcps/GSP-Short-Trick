5a13f48cb5c741bd487bacc39739bc4e52a2bef5
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-24 14:35:32 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Consuming Customer Specific Datasets from Data Sharing Partners using BigQuery/techcps2.sh
+++ Consuming Customer Specific Datasets from Data Sharing Partners using BigQuery/techcps2.sh
@@ -6,17 +6,13 @@ source_dataset_id = "data_publisher_dataset"
 source_dataset_id_full = "{}.{}".format(client.project, source_dataset_id)
 source_dataset = bigquery.Dataset(source_dataset_id_full)
 view_id_a = "$DEVSHELL_PROJECT_ID.data_publisher_dataset.authorized_view"
-
 view_a = bigquery.Table(view_id_a)
-
 view_a.view_query = f"SELECT * FROM \`$SHARED_ID.demo_dataset.authorized_table\` WHERE state_code='NY' LIMIT 1000"
 view_a = client.create_table(view_a)
-
 access_entries = source_dataset.access_entries
 access_entries.append(
 bigquery.AccessEntry(None, "view", view_a.reference.to_api_repr())
 )
-
 source_dataset.access_entries = access_entries
 source_dataset = client.update_dataset(
 source_dataset, ["access_entries"]
