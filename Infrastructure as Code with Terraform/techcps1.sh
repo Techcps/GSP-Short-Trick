a4695d9cfebe94094488ffda4407ccef25fac102
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-31 15:39:17 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Infrastructure as Code with Terraform/techcps1.sh
@@ -0,0 +1,37 @@
+cat > main.tf << EOF_CP
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "$PROJECT_ID"
+  region  = "$REGION"
+  zone    = "$ZONE"
+}
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "e2-medium"
+
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  allow_stopping_for_update = true
+}
+EOF_CP
+
+terraform plan
+terraform apply -auto-approve
+terraform destroy -auto-approve
