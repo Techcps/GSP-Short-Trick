c0058180ccd5ef0ac2f08ca0029cc47970a27e0c
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-01 17:01:58 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Detect Manufacturing Defects using Visual Inspection AI: Challenge Lab/techcpsgsp366.sh
+++ Detect Manufacturing Defects using Visual Inspection AI: Challenge Lab/techcpsgsp366.sh
@@ -1,22 +1,32 @@
 
+export PROJECT_ID=$(gcloud config get-value core/project)
 
-docker run -d --name $CONTAINER_NAME -p 9000:8602 -p 3006:8603 $CONTAINER_REGISTRY
+export container_registry=gcr.io/ql-shared-resources-test/defect_solution@sha256:776fd8c65304ac017f5b9a986a1b8189695b7abbff6aa0e4ef693c46c7122f4c
 
+export VISERVING_CPU_DOCKER_WITH_MODEL=${container_registry}
+export HTTP_PORT=8602
+export LOCAL_METRIC_PORT=8603
+ 
+docker pull ${VISERVING_CPU_DOCKER_WITH_MODEL}
+ 
+docker run -v /secrets:/secrets --rm -d --name $container_name \
+--network="host" \
+-p ${HTTP_PORT}:8602 \
+-p ${LOCAL_METRIC_PORT}:8603 \
+-t ${VISERVING_CPU_DOCKER_WITH_MODEL}
+ 
 docker container ls
 
 gsutil cp gs://cloud-training/gsp895/prediction_script.py .
 
-export PROJECT_ID=$(gcloud config get-value core/project)
 gsutil mb gs://${PROJECT_ID}
 gsutil -m cp gs://cloud-training/gsp897/cosmetic-test-data/*.png \
 gs://${PROJECT_ID}/cosmetic-test-data/
 gsutil cp gs://${PROJECT_ID}/cosmetic-test-data/IMG_07703.png .
 
+python3 ./prediction_script.py --input_image_file=./IMG_07703.png  --port=8602 --output_result_file="$defective"
 
-python3 ./prediction_script.py --input_image_file=./IMG_07703.png  --port=8602 --output_result_file=$DEFECTIVE_NAME
-
-export PROJECT_ID=$(gcloud config get-value core/project)
 gsutil cp gs://${PROJECT_ID}/cosmetic-test-data/IMG_0769.png .
 
-python3 ./prediction_script.py --input_image_file=./IMG_0769.png  --port=8602 --output_result_file=$NON_DEFECTIVE_NAME
+python3 ./prediction_script.py --input_image_file=./IMG_0769.png  --port=8602 --output_result_file="$non_defective"
 
