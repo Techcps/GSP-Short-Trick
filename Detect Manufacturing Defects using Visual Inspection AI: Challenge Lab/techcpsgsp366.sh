29d29b9c8486cd42df949c298fcb81c9650c1255
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-01 14:33:13 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Detect Manufacturing Defects using Visual Inspection AI: Challenge Lab/techcpsgsp366.sh
@@ -0,0 +1,20 @@
+
+
+docker run -d --name $CONTAINER_NAME -p 9000:8602 -p 3006:8603 $CONTAINER_REGISTRY
+
+docker container ls
+
+gsutil cp gs://cloud-training/gsp895/prediction_script.py .
+
+gsutil mb gs://${DEVSHELL _PROJECT_ID}
+
+gsutil -m cp gs://cloud-training/gsp897/cosmetic-test-data/*.png \
+gs://${DEVSHELL _PROJECT_ID}/cosmetic-test-data/
+gsutil cp gs://${DEVSHELL _PROJECT_ID}/cosmetic-test-data/IMG_07703.png .
+
+python3 ./prediction_script.py --input_image_file=./IMG_07703.png  --port=8602 --output_result_file=$DEFECTIVE_NAME
+
+gsutil cp gs://${DEVSHELL _PROJECT_ID}/cosmetic-test-data/IMG_0769.png .
+
+python3 ./prediction_script.py --input_image_file=./IMG_0769.png  --port=8602 --output_result_file=$NON_DEFECTIVE_NAME
+
