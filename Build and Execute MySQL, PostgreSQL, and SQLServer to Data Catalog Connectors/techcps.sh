ff4a43edb3a0c976f6823951ef2045a2c3000457
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-02 08:17:14 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Build and Execute MySQL, PostgreSQL, and SQLServer to Data Catalog Connectors/techcps.sh
+++ Build and Execute MySQL, PostgreSQL, and SQLServer to Data Catalog Connectors/techcps.sh
@@ -1,6 +1,9 @@
 
 gcloud auth list
+
 gcloud services enable datacatalog.googleapis.com --project=$DEVSHELL_PROJECT_ID
+
+export PROJECT_ID=$(gcloud config get-value project)
 
 gsutil cp gs://spls/gsp814/cloudsql-sqlserver-tooling.zip .
 unzip cloudsql-sqlserver-tooling.zip
@@ -13,6 +16,84 @@ sed -i "s/us-central1/$REGION/g" variables.tf
 
 while true; do
     cd ~/cloudsql-sqlserver-tooling
+    bash init-db.sh
+
+    # Check if the init-db.sh script ran successfully
+    if [ $? -eq 0 ]; then
+        echo "Initialization successful. subscribe to techcps"
+        break
+    else
+        echo "Error: Failed to load 'tfplan' Re-running the init-db script."
+    fi
+done
+
+sleep 60 
+
+gcloud iam service-accounts create sqlserver2dc-credentials \
+--display-name  "Service Account for SQL Server to Data Catalog connector" \
+--project $PROJECT_ID
+
+gcloud iam service-accounts keys create "sqlserver2dc-credentials.json" \
+--iam-account "sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
+
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
+--quiet \
+--project $PROJECT_ID \
+--role "roles/datacatalog.admin"
+
+sleep 30
+
+cd infrastructure/terraform/
+
+public_ip_address=$(terraform output -raw public_ip_address)
+username=$(terraform output -raw username)
+password=$(terraform output -raw password)
+database=$(terraform output -raw db_name)
+
+cd ~/cloudsql-sqlserver-tooling
+
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/sqlserver2datacatalog:stable \
+--datacatalog-project-id=$PROJECT_ID \
+--datacatalog-location-id=$REGION \
+--sqlserver-host=$public_ip_address \
+--sqlserver-user=$username \
+--sqlserver-pass=$password \
+--sqlserver-database=$database
+
+
+sleep 17
+
+./cleanup-db.sh
+
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/sqlserver-datacatalog-cleaner:stable \
+--datacatalog-project-ids=$PROJECT_ID \
+--rdbms-type=sqlserver \
+--table-container-type=schema
+
+
+./delete-db.sh
+
+sleep 5 
+
+cd
+
+gsutil cp gs://spls/gsp814/cloudsql-postgresql-tooling.zip .
+unzip cloudsql-postgresql-tooling.zip
+
+cd cloudsql-postgresql-tooling/infrastructure/terraform
+
+
+sed -i "s/us-central1/$REGION/g" variables.tf
+
+
+while true; do
+    cd ~/cloudsql-postgresql-tooling
     bash init-db.sh
 
     # Check if the init-db.sh script ran successfully
