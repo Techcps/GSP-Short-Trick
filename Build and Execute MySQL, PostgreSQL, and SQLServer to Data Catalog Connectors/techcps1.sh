004ad8557e42cbbe84eecbc3428d01b202a06b0b
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-04 23:11:24 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Build and Execute MySQL, PostgreSQL, and SQLServer to Data Catalog Connectors/techcps1.sh
+++ Build and Execute MySQL, PostgreSQL, and SQLServer to Data Catalog Connectors/techcps1.sh
@@ -1,45 +1,4 @@
 
-export PROJECT_ID=$(gcloud config get-value project)
-
-sleep 60 
-
-gcloud iam service-accounts create sqlserver2dc-credentials \
---display-name  "Service Account for SQL Server to Data Catalog connector" \
---project $PROJECT_ID
-
-gcloud iam service-accounts keys create "sqlserver2dc-credentials.json" \
---iam-account "sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
-
-
-gcloud projects add-iam-policy-binding $PROJECT_ID \
---member "serviceAccount:sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
---quiet \
---project $PROJECT_ID \
---role "roles/datacatalog.admin"
-
-sleep 30
-
-cd infrastructure/terraform/
-
-public_ip_address=$(terraform output -raw public_ip_address)
-username=$(terraform output -raw username)
-password=$(terraform output -raw password)
-database=$(terraform output -raw db_name)
-
-cd ~/cloudsql-sqlserver-tooling
-
-
-docker run --rm --tty -v \
-"$PWD":/data mesmacosta/sqlserver2datacatalog:stable \
---datacatalog-project-id=$PROJECT_ID \
---datacatalog-location-id=$REGION \
---sqlserver-host=$public_ip_address \
---sqlserver-user=$username \
---sqlserver-pass=$password \
---sqlserver-database=$database
-
-
-sleep 17
 
 ./cleanup-db.sh
 
