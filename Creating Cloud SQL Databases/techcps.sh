4fbcb99a3fe5e3c95f7b0ecfd053297d9db95c13
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-06 16:11:13 +0530

    Please like share & subscribe to [Techcps](https://www.youtube.com/@techcps)

--- Creating Cloud SQL Databases/techcps.sh
+++ Creating Cloud SQL Databases/techcps.sh
@@ -4,14 +4,14 @@ gcloud services enable sqladmin.googleapis.com --project=$DEVSHELL_PROJECT_ID
 
 gcloud sql instances create postgresql-db --zone=$ZONE --database-version=POSTGRES_14 --tier=db-custom-1-3840 --root-password=techcps@123 --edition=ENTERPRISE
 
-
 gcloud sql databases create petsdb --instance=postgresql-db
-
 
 gcloud sql instances create mysql-db --tier=db-n1-standard-1 --zone=$ZONE
 
 gcloud compute instances create test-client  --zone=$ZONE --image-family=debian-11 --image-project=debian-cloud --machine-type=e2-micro
 
+INSTANCE_NAME="mysql-db"
+EXTERNAL=$(gcloud compute instances list --format='value(EXTERNAL_IP)')
 
 gcloud sql instances patch $INSTANCE_NAME --authorized-networks=$EXTERNAL --quiet
 
