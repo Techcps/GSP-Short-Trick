ad3fbd846391f1118245323ba8a18a994f3f9f4d
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-15 14:10:13 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Cloud Logging on Kubernetes Engine/techcps483.sh
@@ -0,0 +1,39 @@
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set project $PROJECT_ID
+
+git clone https://github.com/GoogleCloudPlatform/gke-logging-sinks-demo
+
+cd gke-logging-sinks-demo
+
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
+
+sed -i 's/  version = "~> 2.11.0"/  version = "~> 2.19.0"/g' terraform/provider.tf
+
+sed -i 's/  filter      = "resource.type = container"/  filter      = "resource.type = k8s_container"/g' terraform/main.tf
+
+make create
+make validate
+
+
+gcloud logging read "resource.type=k8s_container AND resource.labels.cluster_name=stackdriver-logging" --project=$PROJECT_ID
+
+
+gcloud logging read "resource.type=k8s_container AND resource.labels.cluster_name=stackdriver-logging" --project=$PROJECT_ID --format=json
+
+
+gcloud logging sinks create techcps \
+    bigquery.googleapis.com/projects/$PROJECT_ID/datasets/bq_logs \
+    --log-filter='resource.type="k8s_container" 
+resource.labels.cluster_name="stackdriver-logging"' \
+    --include-children \
+    --format='json'
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT * FROM \`$PROJECT_ID.gke_logs_dataset.diagnostic_log_*\`
+WHERE _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', CURRENT_DATE() - INTERVAL 1 DAY) AND FORMAT_DATE('%Y%m%d', CURRENT_DATE()) 
+"
