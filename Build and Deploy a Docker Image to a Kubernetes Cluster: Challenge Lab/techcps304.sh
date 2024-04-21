e43eac463849be092d5c6fe9dcee8cfa5ce310c1
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-21 10:55:47 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Build and Deploy a Docker Image to a Kubernetes Cluster: Challenge Lab/techcps304.sh
+++ Build and Deploy a Docker Image to a Kubernetes Cluster: Challenge Lab/techcps304.sh
@@ -1,20 +1,19 @@
 
-
 export PROJECT_ID=$(gcloud config get-value project)
 
 gcloud container clusters create echo-cluster --project $PROJECT_ID --zone=$ZONE --machine-type=e2-standard-2 --num-nodes=2
 
-gsutil cp gs://${PROJECT_ID}/echo-web.tar.gz .
+gsutil cp gs://sureskills-ql/challenge-labs/ch04-kubernetes-app-deployment/echo-web.tar.gz .
 
-tar -xvzf echo-web.tar.gz
+tar -xvf echo-web.tar.gz
 
-cd echo-web
+gcloud builds submit --tag gcr.io/$PROJECT_ID/echo-app:v1 .
 
-gcloud builds submit --tag gcr.io/{$PROJECT_ID}/echo-app:v1 .
-
-docker push gcr.io/${PROJECT_ID}/echo-app:v1
-
-kubectl create deployment echo-web --image=gcr.io/$PROJECT_ID/echo-web:v1 --port=8000
+kubectl create deployment echo-web --image=gcr.io/qwiklabs-resources/echo-app:v1
+deployment.apps/echo-web created
 
 kubectl expose deployment echo-web --type=LoadBalancer --port=80 --target-port=8000
+service/echo-web exposed
+
+kubectl get svc -w
 
