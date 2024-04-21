e049c77f017ca385209010426ace89070fdc9479
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-21 09:45:10 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Build and Deploy a Docker Image to a Kubernetes Cluster: Challenge Lab/techcps304.sh
+++ Build and Deploy a Docker Image to a Kubernetes Cluster: Challenge Lab/techcps304.sh
@@ -1,8 +1,8 @@
 
 
-export PROJECT_ID=$(gcloud info --format='value(config.project)')
+export PROJECT_ID=$(gcloud config get-value project)
 
-gcloud container clusters create echo-cluster --project $DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-standard-2 --num-nodes=2
+gcloud container clusters create echo-cluster --project $PROJECT_ID --zone=$ZONE --machine-type=e2-standard-2 --num-nodes=2
 
 gsutil cp gs://${PROJECT_ID}/echo-web.tar.gz .
 
@@ -10,16 +10,11 @@ tar -xvzf echo-web.tar.gz
 
 cd echo-web
 
-docker build -t echo-app:v1 .
-
-docker tag echo-app:v1 gcr.io/${PROJECT_ID}/echo-app:v1
+gcloud builds submit --tag gcr.io/{$PROJECT_ID}/echo-app:v1 .
 
 docker push gcr.io/${PROJECT_ID}/echo-app:v1
 
-
 kubectl create deployment echo-web --image=gcr.io/$PROJECT_ID/echo-web:v1 --port=8000
 
-
 kubectl expose deployment echo-web --type=LoadBalancer --port=80 --target-port=8000
-
 
