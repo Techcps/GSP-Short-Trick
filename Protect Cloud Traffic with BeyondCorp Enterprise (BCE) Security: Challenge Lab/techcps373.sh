feca33dc01a7a08760822ff2184fb2249b5d1ef8
Techcps <104138529+Techcps@users.noreply.github.com>
2024-05-03 11:13:50 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Protect Cloud Traffic with BeyondCorp Enterprise (BCE) Security: Challenge Lab/techcps373.sh
@@ -0,0 +1,14 @@
+
+ 
+gcloud auth list
+
+gcloud services enable iap.googleapis.com
+
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+cd python-docs-samples/appengine/standard_python3/hello_world/
+gcloud app create --project=$(gcloud config get-value project) --region=$REGION
+
+gcloud app deploy --quiet
+
