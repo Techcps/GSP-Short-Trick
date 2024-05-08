62310c519731522301c885da23c8bf12a66b47da
Techcps <104138529+Techcps@users.noreply.github.com>
2024-05-08 13:24:26 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Stream Processing with Cloud PubSub and Dataflow: Qwik Start/techcps903.sh
+++ Stream Processing with Cloud PubSub and Dataflow: Qwik Start/techcps903.sh
@@ -1,41 +1,64 @@
 
 
-gcloud services enable dataflow.googleapis.com
+gcloud config set compute/region $REGION
+
+gcloud services disable dataflow.googleapis.com
+gcloud services enable \
+dataflow.googleapis.com \
+cloudscheduler.googleapis.com
+
+
+sleep 45
+
 
 PROJECT_ID=$(gcloud config get-value project)
 BUCKET_NAME="${PROJECT_ID}-bucket"
 TOPIC_ID=my-id
-export REGION=$REGION
 
-AE_REGION=$REGION
 
 gsutil mb gs://$BUCKET_NAME
 
 gcloud pubsub topics create $TOPIC_ID
 
-gcloud app create --region=$AE_REGION
 
+# Set the App Engine region variable
+if [ "$REGION" == "us-central1" ]; then
+  gcloud app create --region us-central
+elif [ "$REGION" == "europe-west1" ]; then
+  gcloud app create --region europe-west
+else
+  gcloud app create --region "$REGION"
+fi
+ 
 
 gcloud scheduler jobs create pubsub publisher-job --schedule="* * * * *" \
-    --topic=$TOPIC_ID --message-body="Hello!" --quiet
+    --topic=$TOPIC_ID --message-body="Hello!"
 
 
-gcloud scheduler jobs run publisher-job
-
-git clone https://github.com/GoogleCloudPlatform/java-docs-samples.git
-cd java-docs-samples/pubsub/streaming-analytics
+gcloud scheduler jobs run publisher-job --location=$REGION
 
 
-mvn compile exec:java \
--Dexec.mainClass=com.examples.pubsub.streaming.PubSubToGcs \
--Dexec.cleanupDaemonThreads=false \
--Dexec.args=" \
-    --project=$PROJECT_ID \
-    --region=$REGION \
-    --inputTopic=projects/$PROJECT_ID/topics/$TOPIC_ID \
-    --output=gs://$BUCKET_NAME/samples/output \
-    --runner=DataflowRunner \
-    --windowSize=2"
 
+cat > techcps.sh <<EOF_CP
+#!/bin/bash
+
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+cd python-docs-samples/pubsub/streaming-analytics
+pip install -U -r requirements.txt
+python PubSubToGCS.py \
+--project=$PROJECT_ID \
+--region=$REGION \
+--input_topic=projects/$PROJECT_ID/topics/$TOPIC_ID \
+--output_path=gs://$BUCKET_NAME/samples/output \
+--runner=DataflowRunner \
+--window_size=2 \
+--num_shards=2 \
+--temp_location=gs://$BUCKET_NAME/temp
+EOF_CP
+
+chmod +x techcps.sh
+
+
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e REGION=$REGION -e TOPIC_ID=$TOPIC_ID -v $(pwd)/techcps.sh:/techcps.sh python:3.7 /bin/bash -c "/techcps.sh"
 
 
