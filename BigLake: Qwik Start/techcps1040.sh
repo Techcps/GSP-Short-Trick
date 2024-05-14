94ea682417a7dd40adc702968604c18e22a84556
Techcps <104138529+Techcps@users.noreply.github.com>
2024-05-14 21:49:59 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ BigLake: Qwik Start/techcps1040.sh
@@ -0,0 +1,44 @@
+
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+
+bq mk --location=us --dataset $DEVSHELL_PROJECT_ID:demo_dataset
+
+
+bq mk --connection --location=US --project_id=$PROJECT_ID --connection_type=CLOUD_RESOURCE my-connection
+
+
+SERVICE_ACCOUNT=$(bq show --format=json --connection $PROJECT_ID.US.my-connection | jq -r '.cloudResource.serviceAccountId')
+
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=serviceAccount:$SERVICE_ACCOUNT \
+  --role=roles/storage.objectViewer
+
+
+export PROJECT_ID=$(gcloud config get-value project)
+bq mkdef \
+--autodetect \
+--connection_id=$PROJECT_ID.US.my-connection \
+--source_format=CSV \
+"gs://$PROJECT_ID/invoice.csv" > /tmp/tabledef.json
+
+
+bq mk --external_table_definition=/tmp/tabledef.json --project_id=$DEVSHELL_PROJECT_ID demo_dataset.biglake_table
+
+
+bq mk --external_table_definition=/tmp/tabledef.json --project_id=$DEVSHELL_PROJECT_ID demo_dataset.external_table
+
+
+bq show --schema --format=prettyjson  demo_dataset.external_table > /tmp/schema
+
+
+bq update --external_table_definition=/tmp/tabledef.json --schema=/tmp/schema demo_dataset.external_table
+
+
+bq query --nouse_legacy_sql "SELECT * FROM \`$DEVSHELL_PROJECT_ID.demo_dataset.biglake_table\`"
+
+
+bq query --nouse_legacy_sql "SELECT * FROM \`$DEVSHELL_PROJECT_ID.demo_dataset.biglake_table\`"
+
