5ed82e53c4e6863bfd47e4600ea3ae04f3d7a062
Techcps <104138529+Techcps@users.noreply.github.com>
2024-05-23 09:23:37 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Designing and Querying Bigtable Schemas/techcps.sh
@@ -0,0 +1,10 @@
+
+
+cbt set test-sessions green1939#1638940844260 Interactions:red_hat=seen
+
+cbt set test-sessions blue2737#1638940844260 Sales:sale=blue_dress#blue_jacket
+
+cbt read test-sessions
+
+cbt deletetable test-sessions
+
