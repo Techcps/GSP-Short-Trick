4a8f4334e3a18f871c770dff0b3507792fa0d3b8
Techcps <104138529+Techcps@users.noreply.github.com>
2024-05-23 22:26:34 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Ingesting DICOM Data with the Healthcare API/techcps615.sh
@@ -0,0 +1,53 @@
+
+
+gcloud services enable healthcare.googleapis.com
+
+
+gcloud healthcare datasets create $DATASET_ID \
+--location=$REGION
+
+sleep 30
+
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+export PROJECT_NUMBER=$(gcloud projects list --filter=projectId:$PROJECT_ID --format="value(projectNumber)")
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com --role=roles/bigquery.admin
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/storage.objectAdmin
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/healthcare.datasetAdmin
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/healthcare.dicomStoreAdmin
+
+gcloud healthcare dicom-stores create $DICOM_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$REGION
+
+
+curl -X POST \
+     -H "Authorization: Bearer "$(sudo gcloud auth print-access-token) \
+     -H "Content-Type: application/json; charset=utf-8" \
+"https://healthcare.googleapis.com/v1beta1/projects/$PROJECT_ID/locations/$REGION/datasets/$DATASET_ID/dicomStores?dicomStoreId=dicomstore2"
+
+
+gcloud healthcare dicom-stores import gcs $DICOM_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$REGION \
+  --gcs-uri=gs://gcs-public-data--healthcare-nih-chest-xray/dicom/000000*.dcm
+
+
+bq mk --dataset --location="$REGION" --description DICOM $PROJECT_ID:$DATASET_ID
+
+curl -X POST \
+    -H "Authorization: Bearer "$(gcloud auth print-access-token) \
+    -H "Content-Type: application/json; charset=utf-8" \
+    --data "{
+      'bigqueryDestination': {
+        'tableUri': 'bq://$PROJECT_ID.$DATASET_ID.$DICOM_STORE_ID'
+      }
+    }" "https://healthcare.googleapis.com/v1beta1/projects/$PROJECT_ID/locations/$REGION/datasets/$DATASET_ID/dicomStores/$DICOM_STORE_ID:export"
+
+    
