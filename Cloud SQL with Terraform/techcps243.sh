c46fd7653fe77a0da3a1bfc18df204bfa995b96e
Techcps <104138529+Techcps@users.noreply.github.com>
2024-06-01 21:35:07 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Cloud SQL with Terraform/techcps243.sh
@@ -0,0 +1,21 @@
+
+
+Cloud SQL with Terraform
+
+
+mkdir sql-with-terraform
+cd sql-with-terraform
+gsutil cp -r gs://spls/gsp234/gsp234.zip .
+
+
+unzip gsp234.zip
+
+
+terraform init
+
+
+terraform plan -out=tfplan
+
+
+terraform apply tfplan
+
