c3e8d9624910adc3f9964c222d2d35ac154ab3f5
Techcps <104138529+Techcps@users.noreply.github.com>
2024-06-05 15:14:56 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Identify Application Vulnerabilities with Security Command Center/techcps.sh
@@ -0,0 +1,44 @@
+
+
+
+
+export ZONE=us-central1-a
+
+export REGION="${ZONE%-*}"
+
+gcloud services enable websecurityscanner.googleapis.com
+
+gcloud compute addresses create xss-test-ip-address --region=$REGION
+
+gcloud compute addresses describe xss-test-ip-address \
+--region=$REGION --format="value(address)"
+
+gcloud compute instances create xss-test-vm-instance \
+--address=xss-test-ip-address --no-service-account \
+--no-scopes --machine-type=e2-micro --zone=$ZONE \
+--metadata=startup-script='apt-get update; apt-get install -y python3-flask'
+
+gcloud compute firewall-rules create enable-wss-scan \
+--direction=INGRESS --priority=1000 \
+--network=default --action=ALLOW \
+--rules=tcp:8080 --source-ranges=0.0.0.0/0
+
+sleep 15
+
+CP_IP=$(gcloud compute instances describe xss-test-vm-instance --zone=$ZONE --project=$DEVSHELL_PROJECT_ID --format="get(networkInterfaces[0].accessConfigs[0].natIP)")
+
+gcloud alpha web-security-scanner scan-configs create --display-name=techcps --starting-urls=http://$CP_IP:8080
+
+
+SCAN_CONFIG=$(gcloud alpha web-security-scanner scan-configs list --project=$DEVSHELL_PROJECT_ID --format="value(name)")
+
+gcloud alpha web-security-scanner scan-runs start $SCAN_CONFIG
+
+sleep 5
+
+
+gcloud compute ssh xss-test-vm-instance --zone $ZONE --project=$DEVSHELL_PROJECT_ID --quiet --command "gsutil cp gs://cloud-training/GCPSEC-ScannerAppEngine/flask_code.tar  . && tar xvf flask_code.tar && python3 app.py"
+
+
+
+
