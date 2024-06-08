8604cc19d6d44dd057597f55709c647cc4443e5c
Techcps <104138529+Techcps@users.noreply.github.com>
2024-06-08 20:05:32 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Speech to Text Transcription with the Cloud Speech API/techcps.sh
+++ Speech to Text Transcription with the Cloud Speech API/techcps.sh
@@ -26,20 +26,3 @@ curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json 
 "https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
 cat result.json
 
-
-cat > request.json <<EOF_CP
- {
-  "config": {
-      "encoding":"FLAC",
-      "languageCode": "fr"
-  },
-  "audio": {
-      "uri":"gs://cloud-samples-data/speech/corbeau_renard.flac"
-  }
-}
-EOF_CP
-
-curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
-"https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
-cat result.json
-
