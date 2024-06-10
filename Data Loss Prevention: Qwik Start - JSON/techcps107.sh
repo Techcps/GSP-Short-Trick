af8d983f0e2db38819c737e41943013186d7a238
Techcps <104138529+Techcps@users.noreply.github.com>
2024-06-11 05:27:52 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Data Loss Prevention: Qwik Start - JSON/techcps107.sh
@@ -0,0 +1,73 @@
+
+
+gcloud auth list
+
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+cat > inspect-request.json <<EOF_CP
+{
+  "item":{
+    "value":"My phone number is (206) 555-0123."
+  },
+  "inspectConfig":{
+    "infoTypes":[
+      {
+        "name":"PHONE_NUMBER"
+      },
+      {
+        "name":"US_TOLLFREE_PHONE_NUMBER"
+      }
+    ],
+    "minLikelihood":"POSSIBLE",
+    "limits":{
+      "maxFindingsPerItem":0
+    },
+    "includeQuote":true
+  }
+}
+EOF_CP
+
+
+curl -s \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:inspect \
+  -d @inspect-request.json -o inspect-output.txt
+
+gsutil cp inspect-output.txt gs://$BUCKET_NAME
+
+
+cat > new-inspect-file.json <<EOF_CP
+{
+  "item": {
+     "value":"My email is test@gmail.com",
+   },
+   "deidentifyConfig": {
+     "infoTypeTransformations":{
+          "transformations": [
+            {
+              "primitiveTransformation": {
+                "replaceWithInfoTypeConfig": {}
+              }
+            }
+          ]
+        }
+    },
+    "inspectConfig": {
+      "infoTypes": {
+        "name": "EMAIL_ADDRESS"
+      }
+    }
+}
+EOF_CP
+
+curl -s \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:deidentify \
+  -d @new-inspect-file.json -o redact-output.txt
+
+
+gsutil cp redact-output.txt gs://$BUCKET_NAME
+
+
