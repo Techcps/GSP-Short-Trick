ffa133f8950819875a396d8fd57bd0ccb6ef11ad
Techcps <104138529+Techcps@users.noreply.github.com>
2024-08-08 14:18:03 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Speech-to-Text API: Qwik Start/techcps119.sh
+++ Speech-to-Text API: Qwik Start/techcps119.sh
@@ -1,14 +1,5 @@
 
-# Google Cloud Speech API: Qwik Start [GSP119]
-In the Navigation menu, select Compute Engine. Click on the SSH button in line with the linux-instance. You will be brought to an interactive shell.
 
-
-
-```
-export API_KEY=
-```
-
-```
 cat > request.json <<EOF
 {
   "config": {
@@ -26,12 +17,4 @@ curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json 
 
 curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
 "https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
-
-```
-
-## Congratulations, you're all done with the lab 😄 Don't forget to subscribe my YouTube Channel😄
-
-##  Thanks for watching.!
-
-
 
