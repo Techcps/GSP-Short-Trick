2d635ff0fbdbe5e3bd7aac92eeb87786f0f8f90a
Techcps <104138529+Techcps@users.noreply.github.com>
2024-08-12 15:17:46 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Cloud Functions: Qwik Start - Command Line/techcps080.sh
+++ Cloud Functions: Qwik Start - Command Line/techcps080.sh
@@ -1,44 +1,72 @@
+#New!
 
 
 gcloud auth list
-gcloud config list project
 
-gcloud services disable cloudfunctions.googleapis.com
-gcloud services enable cloudfunctions.googleapis.com
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+gcloud config set project $DEVSHELL_PROJECT_ID
 
+gcloud services disable cloudfunctions.googleapis.com --project=$DEVSHELL_PROJECT_ID
+gcloud services enable cloudfunctions.googleapis.com --project=$DEVSHELL_PROJECT_ID
 
 gcloud config set compute/region $REGION
+
 mkdir gcf_hello_world
 
+
 cd gcf_hello_world
-cat > index.js << EOF_CP
-/**
-* Background Cloud Function to be triggered by Pub/Sub.
-* This function is exported by index.js, and executed when
-* the trigger topic receives a message.
-*
-* @param {object} data The event payload.
-* @param {object} context The event metadata.
-*/
-exports.helloWorld = (data, context) => {
-const pubSubMessage = data;
-const name = pubSubMessage.data
-    ? Buffer.from(pubSubMessage.data, 'base64').toString() : "Hello World";
-    
-console.log("My Cloud Function: "+name);
-};
+
+cat > index.js <<'EOF_CP'
+const functions = require('@google-cloud/functions-framework');
+
+// Register a CloudEvent callback with the Functions Framework that will
+// be executed when the Pub/Sub trigger topic receives a message.
+functions.cloudEvent('helloPubSub', cloudEvent => {
+  // The Pub/Sub message is passed as the CloudEvent's data payload.
+  const base64name = cloudEvent.data.message.data;
+
+  const name = base64name
+    ? Buffer.from(base64name, 'base64').toString()
+    : 'World';
+
+  console.log(`Hello, ${name}!`);
+});
 EOF_CP
 
-gsutil mb -p $DEVSHELL_PROJECT_ID gs://$DEVSHELL_PROJECT_ID
+
+cat > package.json <<'EOF_CP'
+{
+  "name": "gcf_hello_world",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "dependencies": {
+    "@google-cloud/functions-framework": "^3.0.0"
+  }
+}
+EOF_CP
+
 
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
 --member="serviceAccount:$DEVSHELL_PROJECT_ID@appspot.gserviceaccount.com" \
 --role="roles/artifactregistry.reader"
 
-#!/bin/bash
 
 deploy_function() {
-  gcloud functions deploy helloWorld --stage-bucket $DEVSHELL_PROJECT_ID --trigger-topic hello_world --runtime nodejs20
+  gcloud functions deploy nodejs-pubsub-function \
+  --gen2 \
+  --runtime=nodejs20 \
+  --region=$REGION \
+  --source=. \
+  --entry-point=helloPubSub \
+  --trigger-topic cf-demo \
+  --stage-bucket $DEVSHELL_PROJECT_ID-bucket \
+  --service-account cloudfunctionsa@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
+  --allow-unauthenticated --quiet
 }
 
 deploy_success=false
@@ -49,10 +77,13 @@ while [ "$deploy_success" = false ]; do
     deploy_success=true
   else
     echo "Retrying, please subscribe to techcps (https://www.youtube.com/@techcps)..."
-    sleep 30
+    sleep 20
   fi
 done
 
+gcloud functions describe nodejs-pubsub-function \
+  --region=$REGION
 
-gcloud functions describe helloWorld
 
+echo "Congratulations, you're all done with the lab"
+echo "Please like share and subscribe to techcps(https://www.youtube.com/@techcps)..."
