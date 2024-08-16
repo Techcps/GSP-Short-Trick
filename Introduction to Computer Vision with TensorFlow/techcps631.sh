ff029e8c89f508dfc9c803fe46e8d3132891d7b9
Techcps <104138529+Techcps@users.noreply.github.com>
2024-08-16 12:05:01 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Introduction to Computer Vision with TensorFlow/techcps631.sh
+++ Introduction to Computer Vision with TensorFlow/techcps631.sh
@@ -3,11 +3,11 @@ python --version
 
 python -c "import tensorflow;print(tensorflow.__version__)"
 
-pip install tensorflow tensorflow-datasets google-cloud-logging numpy
-
 pip3 install --upgrade pip
 
-/usr/bin/python3 -m pip install -U google-cloud-logging --user
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/requirements.txt
+
+/usr/bin/python3 -m pip install -r requirements.txt
 
 /usr/bin/python3 -m pip install -U pylint --user
 
@@ -15,39 +15,28 @@ pip install --upgrade tensorflow
 
 wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/model.py
 
-wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/callback_model.py
-
-wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_1.py
-
-wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_2.py
-
-wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_3.py
-
-wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_4.py
-
-
 python model.py
+
+sleep 5
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/callback_model.py
 
 python callback_model.py
 
+sleep 5
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_1.py
+
 python updated_model_1.py
+
+sleep 5
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_2.py
 
 python updated_model_2.py
 
+sleep 5
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_3.py
+
 python updated_model_3.py
-
-sleep 30
-
-pip uninstall numpy -y
-
-pip uninstall numpy tensorflow -y
-
-pip install tensorflow==2.10.0 numpy==1.21.6
-
-pip uninstall google-cloud-logging google-api-core google-protobuf -y
-
-pip install google-cloud-logging==2.6.0 google-api-core==1.31.5 protobuf==3.20.1
-
-pip show google-cloud-logging google-api-core google-protobuf
-
-python update_model_4.py
