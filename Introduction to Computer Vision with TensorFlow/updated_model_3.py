18f55fde5aec9ee55ebdfe9d9d3b73cb42953e2d
Techcps <104138529+Techcps@users.noreply.github.com>
2024-08-16 11:53:59 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Introduction to Computer Vision with TensorFlow/updated_model_3.py
+++ Introduction to Computer Vision with TensorFlow/updated_model_3.py
@@ -18,13 +18,17 @@ import tensorflow as tf
 (ds_train, ds_test), info = tfds.load('fashion_mnist', split=['train', 'test'], with_info=True, as_supervised=True)
 # Define batch size
 BATCH_SIZE = 32
+
+
 # Normalizing and batch processing of data
 ds_train = ds_train.batch(BATCH_SIZE)
 ds_test = ds_test.batch(BATCH_SIZE)
+
 # Define the model
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
                                     tf.keras.layers.Dense(64, activation=tf.nn.relu),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
 # Compile data
 model.compile(optimizer = tf.keras.optimizers.Adam(),
               loss = tf.keras.losses.SparseCategoricalCrossentropy(),
