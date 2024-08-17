f289e2f92b260f7626fb23a329d36b228f4d350e
Techcps <104138529+Techcps@users.noreply.github.com>
2024-08-17 12:25:15 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Introduction to Computer Vision with TensorFlow/techcps.sh
@@ -0,0 +1,17 @@
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/callback_model.py
+
+python callback_model.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_1.py
+
+python updated_model_1.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_2.py
+
+python updated_model_2.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_3.py
+
+python updated_model_3.py
+
