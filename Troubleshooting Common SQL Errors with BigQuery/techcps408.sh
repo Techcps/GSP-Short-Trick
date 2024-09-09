b6159e486015dc9b497ee68c6576d61ae5201ad2
Techcps <104138529+Techcps@users.noreply.github.com>
2024-09-09 13:09:32 +0530

    Please like share & subscribe to [Techcps](https://www.youtube.com/@techcps)

--- Troubleshooting Common SQL Errors with BigQuery/techcps408.sh
+++ Troubleshooting Common SQL Errors with BigQuery/techcps408.sh
@@ -87,3 +87,41 @@ WHERE hits_product_v2ProductName IS NOT NULL
 GROUP BY hits_product_v2ProductCategory
 ORDER BY number_of_products DESC
 LIMIT 5'
+
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT hits_product_v2ProductName, hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+GROUP BY 1,2
+"
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+COUNT(hits_product_v2ProductName) as number_of_products,
+hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+WHERE hits_product_v2ProductName IS NOT NULL
+GROUP BY hits_product_v2ProductCategory
+ORDER BY number_of_products DESC
+"
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+COUNT(DISTINCT hits_product_v2ProductName) as number_of_products,
+hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+WHERE hits_product_v2ProductName IS NOT NULL
+GROUP BY hits_product_v2ProductCategory
+ORDER BY number_of_products DESC
+LIMIT 5
+"
+
+
