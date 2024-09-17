5e53a5e8d39efeb6fc70c25969da7ee73dd25e35
Techcps <104138529+Techcps@users.noreply.github.com>
2024-09-17 07:37:04 +0530

    Please like share & subscribe to [Techcps](https://www.youtube.com/@techcps)

--- APIs Explorer: Qwik Start/techcps277.sh
+++ APIs Explorer: Qwik Start/techcps277.sh
@@ -4,11 +4,13 @@ gcloud auth list
 
 export PROJECT_ID=$(gcloud config get-value project)
 
-gsutil mb -p $PROJECT_ID -c regional -l us-east1 gs://$PROJECT_ID-bucket
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket
 
 curl -L -o demo-image.jpg https://github.com/Techcps/GSP-Short-Trick/blob/main/APIs%20Explorer%3A%20Qwik%20Start/demo-image.jpg
 
-gsutil cp demo-image.jpg gs://$PROJECT_ID-bucket/demo-image.jpg
+gsutil cp demo-image.jpg gs://$DEVSHELL_PROJECT_ID-bucket/demo-image.jpg
 
-gsutil acl ch -u allUsers:R gs://$PROJECT_ID-bucket/demo-image.jpg
+gsutil acl ch -u allUsers:R gs://$DEVSHELL_PROJECT_ID-bucket/demo-image.jpg
 
