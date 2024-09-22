670600c18d204f94abe1a6d3d2971b60de5f49ce
Techcps <104138529+Techcps@users.noreply.github.com>
2024-09-22 17:10:38 +0530

    Please like share & subscribe to [Techcps](https://www.youtube.com/@techcps)

--- Clean Up Unused IP Addresses/techcps646.sh
+++ Clean Up Unused IP Addresses/techcps646.sh
@@ -11,9 +11,13 @@ export PROJECT_ID=$(gcloud config get-value project)
 
 export PROJECT_ID=$DEVSHELL_PROJECT_ID
 
-gcloud config set project $DEVSHELL_PROJECT_ID
+gcloud services disable cloudfunctions.googleapis.com
 
-gcloud services enable cloudscheduler.googleapis.com --project=$PROJECT_ID
+gcloud services enable cloudfunctions.googleapis.com 
+
+gcloud services enable run.googleapis.com
+
+gcloud services enable cloudscheduler.googleapis.com
 
 sleep 30
 
@@ -23,9 +27,6 @@ export PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/nul
 WORKDIR=$(pwd)
 
 sleep 10
-
-export PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
-WORKDIR=$(pwd)
 
 cd $WORKDIR/unused-ip
 
@@ -43,58 +44,68 @@ sleep 15
 
 gcloud compute addresses list --filter="region:($REGION)"
 
-
 export USED_IP_ADDRESS=$(gcloud compute addresses describe $USED_IP --region=$REGION --format=json | jq -r '.address')
 
-
 gcloud compute instances create static-ip-instance \
---zone=$ZONE --project=$PROJECT_ID \
+--zone=$ZONE \
+--project=$PROJECT_ID \
 --machine-type=e2-medium \
 --subnet=default \
 --address=$USED_IP_ADDRESS
 
-
 gcloud compute addresses list --filter="region:($REGION)"
 
 cat $WORKDIR/unused-ip/function.js | grep "const compute" -A 31
-
-gcloud services disable cloudfunctions.googleapis.com --project=$PROJECT_ID
-
-sleep 10
-
-gcloud services enable cloudfunctions.googleapis.com --project=$PROJECT_ID
 
 
 gcloud projects add-iam-policy-binding $PROJECT_ID \
 --member="serviceAccount:$PROJECT_ID@appspot.gserviceaccount.com" \
 --role="roles/artifactregistry.reader"
 
-sleep 60
 
+PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format='value(projectNumber)')
 
-gcloud functions deploy unused_ip_function --trigger-http --runtime=nodejs12 --region=$REGION --allow-unauthenticated --quiet
+deploy_function() {
+    gcloud functions deploy unused_ip_function --gen2 --trigger-http --runtime=nodejs20 --region=$REGION --allow-unauthenticated --quiet
+}
+
+SERVICE_NAME="unused_ip_function"
+
+while true; do
+  deploy_function
+  if gcloud functions describe $SERVICE_NAME --region=$REGION &> /dev/null; then
+    echo "Function deployed successfully."
+    break
+  else
+    echo "Retrying, please subscribe to techcps[https://www.youtube.com/@techcps]"
+    sleep 20
+  fi
+done
 
 
 export FUNCTION_URL=$(gcloud functions describe unused_ip_function --region=$REGION --format=json | jq -r '.httpsTrigger.url')
 
+if [ "$REGION" == "us-central1" ]; then
+  gcloud app create --region us-central
+else
+  gcloud app create --region "$REGION"
+fi
+
+
 gcloud app create --region $REGION
-
-
 
 gcloud scheduler jobs create http unused-ip-job \
 --schedule="* 2 * * *" \
 --uri=$FUNCTION_URL \
 --location=$REGION
 
-sleep 30
-
-
+sleep 45 
 
 gcloud scheduler jobs run unused-ip-job \
 --location=$REGION
 
-gcloud compute addresses list --filter="region:($REGION)"
 
+gcloud compute addresses list --filter="region:($REGION)"
 
 
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
@@ -102,12 +113,11 @@ WORKDIR=$(pwd)
 
 cd $WORKDIR/unused-ip
 
-export USED_IP=used-ip-address
 export UNUSED_IP=unused-ip-address
-
 
 gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region=$REGION
 
 sleep 15
 
 gcloud compute addresses list --filter="region:($REGION)"
+
