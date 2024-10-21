1f24e6fed50af8e11a3cf0ad6025a39f6d80d58f
Techcps <104138529+Techcps@users.noreply.github.com>
2024-10-21 10:45:27 +0530

    Watch here https://youtu.be/o5xbAR1rnqA

--- Introduction to APIs in Google Cloud/techcps294.sh
+++ Introduction to APIs in Google Cloud/techcps294.sh
@@ -24,11 +24,23 @@ curl -X POST --data-binary @values.json \
     -H "Authorization: Bearer $OAUTH2_TOKEN" \
     -H "Content-Type: application/json" \
     "https://www.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
+    
 
-# Upload an image to the bucket
-curl -X POST --data-binary @/home/gcpstaging25084_student/demo-image.png \
-    -H "Authorization: Bearer $OAUTH2_TOKEN" \
-    -H "Content-Type: image/png" \
-    "https://www.googleapis.com/upload/storage/v1/b/${DEVSHELL_PROJECT_ID}-bucket/o?uploadType=media&name=demo-image.png"
+wget https://github.com/Techcps/GSP-Short-Trick/raw/master/Introduction%20to%20APIs%20in%20Google%20Cloud/demo-image.png
 
+# Verify if the image is downloaded
+if [ -f demo-image.png ]; then
+    export OBJECT=$(realpath demo-image.png)
 
+    export BUCKET_NAME=$DEVSHELL_PROJECT_ID-bucket
+
+    export OAUTH2_TOKEN=$(gcloud auth print-access-token)
+
+    curl -X POST --data-binary @$OBJECT \
+        -H "Authorization: Bearer $OAUTH2_TOKEN" \
+        -H "Content-Type: image/png" \
+        "https://www.googleapis.com/upload/storage/v1/b/$BUCKET_NAME/o?uploadType=media&name=demo-image"
+else
+    echo "File not downloaded. Check the URL or file path."
+    echo "Please like share and subscribe to techcps(https://www.youtube.com/@techcps)."
+fi
