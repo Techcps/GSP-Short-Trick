5f6897ed7d1834697f547010a393835927015cf3
Techcps <104138529+Techcps@users.noreply.github.com>
2024-11-06 19:01:10 +0530

    Watch Here https://youtu.be/KyC-tYCejhM

--- Mitigate Threats and Vulnerabilities with Security Command Center: Challenge Lab/techcps1.sh
+++ Mitigate Threats and Vulnerabilities with Security Command Center: Challenge Lab/techcps1.sh
@@ -7,6 +7,8 @@ gcloud scc muteconfigs create muting-audit-logging-findings --project=$DEVSHELL_
 
 gcloud scc muteconfigs create muting-admin-sa-findings --project=$DEVSHELL_PROJECT_ID --location=global --description="Rule for muting admin service account findings" --filter="category=\"ADMIN_SERVICE_ACCOUNT\"" --type=STATIC
 
+sleep 20
+
 gcloud compute firewall-rules delete default-allow-rdp  --quiet && gcloud compute firewall-rules delete default-allow-ssh --quiet
 
 gcloud compute firewall-rules create default-allow-rdp --source-ranges=35.235.240.0/20 --allow=tcp:3389 --priority=65534 --description="Allow HTTP traffic from 35.235.240.0/20"
