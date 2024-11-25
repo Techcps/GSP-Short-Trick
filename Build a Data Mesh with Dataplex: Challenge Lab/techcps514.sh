8d1b79e88de52483dceb74a4e1474300aae55081
Techcps <104138529+Techcps@users.noreply.github.com>
2024-11-25 15:18:29 +0530

    Update techcps514.sh

--- Build a Data Mesh with Dataplex: Challenge Lab/techcps514.sh
+++ Build a Data Mesh with Dataplex: Challenge Lab/techcps514.sh
@@ -1,5 +1,17 @@
 
 
+
+# Set text styles
+YELLOW=$(tput setaf 3)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+echo "Please set the below values correctly"
+read -p "${YELLOW}${BOLD}Enter the REGION: ${RESET}" REGION
+read -p "${YELLOW}${BOLD}Enter the USER_ID_2: ${RESET}" USER_ID_2
+
+export REGION USER_ID_2
+
 gcloud auth list
 
 gcloud config set project $DEVSHELL_PROJECT_ID
@@ -10,14 +22,17 @@ sleep 15
 
 gcloud dataplex lakes create sales-lake --location=$REGION --project=$DEVSHELL_PROJECT_ID --display-name="Sales Lake"
 
-Curated zone named Curated Customer Zone
-
-
 gcloud dataplex zones create curated-customer-zone --location=$REGION --lake=sales-lake --display-name="Curated Customer Zone" --resource-location-type=SINGLE_REGION --discovery-enabled --type=CURATED --discovery-schedule="0 * * * *"
-
 
 gcloud dataplex zones create raw-customer-zone --location=$REGION --lake=sales-lake --display-name="Raw Customer Zone" --resource-location-type=SINGLE_REGION --discovery-enabled --type=RAW --discovery-schedule="0 * * * *"
 
+gcloud dataplex assets create customer-engagements --location=$REGION --lake=sales-lake --display-name="Customer Engagements" --resource-type=STORAGE_BUCKET --discovery-enabled --zone=raw-customer-zone --resource-name=projects/$DEVSHELL_PROJECT_ID/buckets/$DEVSHELL_PROJECT_ID-customer-online-sessions
+
+gcloud dataplex assets create customer-orders --location=$REGION --lake=sales-lake --display-name="Customer Orders" --resource-type=BIGQUERY_DATASET --discovery-enabled --zone=curated-customer-zone --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customer_orders \
+  
+gcloud data-catalog tag-templates create protected_customer_data_template --location=$REGION --display-name="Protected Customer Data Template" --field=id=raw_data_flag,display-name="Raw Data Flag",type='enum(Yes|No)',required=TRUE --field=id=protected_contact_information_flag,display-name="Protected Contact Information Flag",type='enum(Yes|No)',required=TRUE
+
+gcloud dataplex assets add-iam-policy-binding customer-engagements --location=$REGION --lake=sales-lake --zone=raw-customer-zone --role=roles/dataplex.dataWriter --member=user:$USER_ID_2
 
 
 cat > dq-customer-orders.yaml <<EOF_CP
@@ -56,13 +71,22 @@ EOF_CP
 
 gsutil cp dq-customer-orders.yaml gs://$DEVSHELL_PROJECT_ID-dq-config
 
-
 echo "Please subscribe to techcps[https://www.youtube.com/@techcps]"
-
-echo "Click the below link"
 
 echo "-------------"
 
-echo "Click here to open the link https://console.cloud.google.com/dataplex/lakes/sales-lake;location=$REGION/zones?cloudshell=true&project=$DEVSHELL_PROJECT_ID"
+echo ""
+
+echo "Click here to open the link https://console.cloud.google.com/dataplex/search?project=$DEVSHELL_PROJECT_ID&q=Raw%20Customer%20Zone&qSystems=DATAPLEX"
+
+echo ""
+
+echo "Click here to open the link https://console.cloud.google.com/dataplex/process/create-task?project=$DEVSHELL_PROJECT_ID"
+
+
+echo ""
+
+echo ""
+
 
 
