cc345b085b43ae949b5739d99d4f2a5ea2236fe7
Techcps <104138529+Techcps@users.noreply.github.com>
2024-12-17 19:59:39 +0530

    Create techcps090.sh

---
+++ Monitoring Multiple Projects with Cloud Monitoring/techcps090.sh
@@ -0,0 +1,52 @@
+
+
+# Set text styles
+YELLOW=$(tput setaf 3)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+echo "Please set the below values correctly"
+read -p "${YELLOW}${BOLD}Enter the ZONE: ${RESET}" ZONE
+
+# Export variables after collecting input
+export ZONE
+
+gcloud compute instances create instance2 --zone=$ZONE --machine-type=e2-medium
+
+
+cat > techcps.json <<EOF_CP
+{
+  "displayName": "Uptime Check Policy",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "VM Instance - Check passed",
+      "conditionAbsent": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"monitoring.googleapis.com/uptime_check/check_passed\" AND metric.labels.check_id = \"demogroup-uptime-check-f-UeocjSHdQ\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "300s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_FRACTION_TRUE"
+          }
+        ],
+        "duration": "300s",
+        "trigger": {
+          "count": 1
+        }
+      }
+    }
+  ],
+  "alertStrategy": {},
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF_CP
+
+
+gcloud alpha monitoring policies create --policy-from-file=techcps.json
+
+
+
