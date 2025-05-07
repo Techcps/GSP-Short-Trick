7ee96479861a866b427152b1fb57205391a0f726
Techcps <104138529+Techcps@users.noreply.github.com>
2025-05-07 05:58:48 +0530

    Delete Google Cloud Pub-Sub: Qwik Start - Console directory

--- Google Cloud Pub-Sub: Qwik Start - Console/techcpsgsp096.sh
+++
@@ -1,9 +0,0 @@
-
-gcloud auth list
-gcloud config list project
-
-gcloud services enable pubsub.googleapis.com
-
-gcloud pubsub topics create MyTopic
-
-gcloud pubsub subscriptions create MySub --topic=MyTopic
