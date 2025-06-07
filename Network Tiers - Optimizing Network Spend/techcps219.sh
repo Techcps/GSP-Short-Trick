eda2fb2ec6995238e17f245e203ff1837bb9f366
Techcps <104138529+Techcps@users.noreply.github.com>
2025-06-07 06:42:27 +0530

    Delete

--- Network Tiers - Optimizing Network Spend/techcps219.sh
+++
@@ -1,3 +0,0 @@
-
-gcloud compute instances create vm-premium --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-tier=PREMIUM --image-family=debian-11 --image-project=debian-cloud && gcloud compute instances create vm-standard --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-tier=STANDARD --image-family=debian-11 --image-project=debian-cloud
-
